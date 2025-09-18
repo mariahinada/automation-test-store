0b86ca67758c2a47beb0bad851a1db27f0a45aa6
Mariah Inada <mariahclarainada@gmail.com>
2025-09-18 14:41:17 -0300

    update: cria helper para limpeza de carrinho

--- cypress/e2e/checkout/checkout.cy.js
+++ cypress/e2e/checkout/checkout.cy.js
@@ -1,6 +1,6 @@
 import { CheckoutService } from "../../services/checkout/checkout-service";
 import { CheckoutExpectations } from "../../expectations/checkout/checkout-expectations";
-import { RandomDataHelper } from "../../helpers/random-data";
+import { CartHelper } from "../../helpers/cart-helper";
 
 describe("Fluxo de Compra no Automation Test Store", () => {
   const service = new CheckoutService();
@@ -8,27 +8,27 @@ describe("Fluxo de Compra no Automation Test Store", () => {
 
   beforeEach(() => {
     cy.visit("/");
-  });
-
-  it("Adicionar produto simples ao carrinho", () => {
     service.clickLoginOrRegisterBtn();
     service.fillLoginName("usuario");
     service.fillPassword("senha123");
     service.clickContinueBtnToLogin();
     service.clickHomeBtn();
+  });
+
+  it("Adicionar produto simples ao carrinho", () => {
     service.clickSimpleProduct();
     service.clickCartBtn();
     expect.showSimpleProductOnCart();
   });
 
+  afterEach(() => {
+    CartHelper.clearCart();
+    cy.log("Carrinho limpo");
+  });
+
   it("Adicionar produto com variação ao carrinho", () => {
-    service.clickLoginOrRegisterBtn();
-    service.fillLoginName("usuario");
-    service.fillPassword("senha123");
-    service.clickContinueBtnToLogin();
-    service.clickHomeBtn();
     service.clickVariationProduct();
-    service.selectVariationOption('664');
+    service.selectVariationOption("664");
     service.clickCartBtn();
     expect.showVariationProductOnCart();
   });
