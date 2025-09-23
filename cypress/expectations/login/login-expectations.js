5415e1e4f8962e8fd19e949f7c0e79f6b5f650fd
Mariah Inada <mariahclarainada@gmail.com>
2025-09-23 11:49:07 -0300

    update: cria expectations para o fluxo de login

---
+++ cypress/expectations/login/login-expectations.js
@@ -0,0 +1,20 @@
+import { AbstractExpectation } from "../abstract-expectations";
+export class LoginExpectations extends AbstractExpectation {
+    
+  // login com sucesso
+  showUserLoggedIn(loginName) {
+    if (!loginName)
+      throw new Error("showUserLoggedIn: loginName não fornecido");
+    this.expectUrlToBe(
+      "https://automationteststore.com/index.php?rt=account/account"
+    );
+  }
+
+  // login sem sucesso (usuário ou senha inválidos)
+  showInvalidLoginOrPassword() {
+    this.expectElementToHaveText(
+      "#maincontainer > div > div > div > div.alert.alert-error.alert-danger",
+      "Error: Incorrect login or password provided."
+    );
+  }
+}
