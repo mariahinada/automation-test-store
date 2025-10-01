aae9f5d918081d22859e7fea13e45ae2c79b68b2
Mariah Inada <mariahclarainada@gmail.com>
2025-10-01 12:15:19 -0300

    chore: adiciona formatação padrão

--- cypress/expectations/forgot-credentials/forgot-credentials-expectations.js
+++ cypress/expectations/forgot-credentials/forgot-credentials-expectations.js
@@ -1,28 +1,26 @@
 import { AbstractExpectation } from "../abstract-expectations";
 export class ForgotCredentialsExpectations extends AbstractExpectation {
+  // mensagem de sucesso para recuperação de senha
+  showSuccessMessageForForgotPassword() {
+    this.expectElementToHaveText(
+      "div.alert.alert-success",
+      "Success: Password reset link has been sent to your e-mail address."
+    );
+  }
 
-    // mensagem de sucesso para recuperação de senha
-    showSuccessMessageForForgotPassword() {
-        this.expectElementToHaveText(
-            "div.alert.alert-success",
-            "Success: Password reset link has been sent to your e-mail address."
-        )
-    }
+  // mensagem de sucesso para recuperação de login
+  showSucessMessageForForgotLogin() {
+    this.expectElementToHaveText(
+      "div.alert.alert-success",
+      "Success: Your login name reminder has been sent to your e-mail address."
+    );
+  }
 
-    // mensagem de sucesso para recuperação de login
-    showSucessMessageForForgotLogin() {
-        this.expectElementToHaveText(
-            "div.alert.alert-success",
-            "Success: Your login name reminder has been sent to your e-mail address."
-
-        )
-    }
-
-    // mensagem de falha para recuperação de senha/login
-    showFailureMessage() {
-        this.expectElementToHaveText(
-            "div.alert.alert-error.alert-danger",
-            "Error: No records found matching information your provided, please check your information and try again!"
-        )
-    }
+  // mensagem de falha para recuperação de senha/login
+  showFailureMessage() {
+    this.expectElementToHaveText(
+      "div.alert.alert-error.alert-danger",
+      "Error: No records found matching information your provided, please check your information and try again!"
+    );
+  }
 }
--- cypress/services/forgot-credentials/forgot-credentials-service.js
+++ cypress/services/forgot-credentials/forgot-credentials-service.js
@@ -1,55 +1,56 @@
 import { AbstractService } from "../abstract-service";
 
 export class ForgotCredentialsService extends AbstractService {
-    elements = {
-        //background
-        loginOrRegisterBtn: "#customer_menu_top > li > a",
+  elements = {
+    //background
+    loginOrRegisterBtn: "#customer_menu_top > li > a",
 
-        // forgot password form
-        forgotPasswordBtn: "a:contains('Forgot your password?')",
-        loginName: "#forgottenFrm_loginname",
-        email: "#forgottenFrm_email",
+    // formulário de recuperação de senha
+    forgotPasswordBtn: "a:contains('Forgot your password?')",
+    loginName: "#forgottenFrm_loginname",
+    email: "#forgottenFrm_email",
 
-        // forgot login form
-        forgotLoginBtn: "a:contains('Forgot your login name')",
-        lastName: "#forgottenFrm_lastname",
+    // formulário de recuperação de login
+    forgotLoginBtn: "a:contains('Forgot your login name')",
+    lastName: "#forgottenFrm_lastname",
 
-        // submit button
-        continueBtnToSubmitForgotCredentials: "#forgottenFrm button:contains('Continue')",
-    }
+    // botão de submit
+    continueBtnToSubmitForgotCredentials:
+      "#forgottenFrm button:contains('Continue')",
+  };
 
-    // background
-    clickLoginOrRegisterBtn() {
-        this.click(this.elements.loginOrRegisterBtn);
-    }
+  // background
+  clickLoginOrRegisterBtn() {
+    this.click(this.elements.loginOrRegisterBtn);
+  }
 
-    // forgot password form
+  // formulário de recuperação de senha
 
-    clickForgotPasswordBtn() {
-        this.click(this.elements.forgotPasswordBtn);
-    }
+  clickForgotPasswordBtn() {
+    this.click(this.elements.forgotPasswordBtn);
+  }
 
-    fillLoginName(loginName) {
-        this.type(this.elements.loginName, loginName)
-    }
+  fillLoginName(loginName) {
+    this.type(this.elements.loginName, loginName);
+  }
 
-    fillEmail(email) {
-        this.type(this.elements.email, email)
-    }
+  fillEmail(email) {
+    this.type(this.elements.email, email);
+  }
 
-    // forgot login form
+  // formulário de recuperação de login
 
-    clickForgotLoginBtn() {
-        this.click(this.elements.forgotLoginBtn);
-    }
+  clickForgotLoginBtn() {
+    this.click(this.elements.forgotLoginBtn);
+  }
 
-    fillLastName(lastName) {
-        this.type(this.elements.lastName, lastName);
-    }
+  fillLastName(lastName) {
+    this.type(this.elements.lastName, lastName);
+  }
 
-    // submit button
+  // botão de submit
 
-    clickContinueBtnToSubmitForgotCredentials() {
-        this.click(this.elements.continueBtnToSubmitForgotCredentials);
-    }
+  clickContinueBtnToSubmitForgotCredentials() {
+    this.click(this.elements.continueBtnToSubmitForgotCredentials);
+  }
 }
