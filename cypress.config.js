f0a09c9229f325526dce7b7e11e412ad02e4d4aa
Mariah Inada <mariahclarainada@gmail.com>
2025-10-02 15:16:46 -0300

    update: abre o navegador pra teste sem usar gpu

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true, // nível global
+  video: true, 
   e2e: {
     baseUrl: "https://automationteststore.com/",
     viewportWidth: 1920,
     viewportHeight: 1080,
     specPattern: "**/*.{cy,spec}.{js,jsx,ts,tsx}",
+
     setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (
+          browser.name === "chrome" ||
+          browser.name === "chromium" ||
+          browser.name === "electron"
+        ) {
+          launchOptions.args.push("--disable-gpu");
+          launchOptions.args.push("--disable-software-rasterizer");
+        }
+        return launchOptions;
+      });
+
       return config;
     },
   },
