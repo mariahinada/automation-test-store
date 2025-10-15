cbb720efdc7a659ee9f75d79bbe283fe243ca007
Mariah Inada <mariahclarainada@gmail.com>
2025-10-15 10:29:49 -0300

    adiciona novas expectations

--- cypress/expectations/abstract-expectations.js
+++ cypress/expectations/abstract-expectations.js
@@ -3,8 +3,20 @@ export class AbstractExpectation {
     cy.get(selector).should("be.visible");
   }
 
+  expectElementToExist(selector) {
+    cy.get(selector).should("exist")
+  }
+
+  expectElementToNotExist(selector) {
+    cy.get(selector).should("not.exist")
+  }
+
   expectElementToHaveText(selector, text) {
     cy.get(selector).should("contain.text", text);
+  }
+
+  expectElementToHaveValue(selector, value) {
+    cy.get(selector).should("have.value", value)
   }
 
   expectUrlToBe(url) {
