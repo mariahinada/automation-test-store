a1a0594b84465cb7829411ffc4b67c47519f40ce
Mariah Inada <115057822+mariahinada@users.noreply.github.com>
2025-10-23 15:24:47 -0300

    refactor: remove arquivos antigos de expectations

--- cypress/expectations/create-account/create-account-success-expectations.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractExpectation } from "../abstract-expectations";
-
-export class CreateAccountSuccessExpectations extends AbstractExpectation {
-  showSuccessMessage() {
-    this.expectElementToHaveText(".maintext", "Your Account Has Been Created!");
-  }
-}
